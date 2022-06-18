c0822cf4a6accc4e7c166adae7573660c240de22
Kanya15 <21897521@mycput.ac.za>
2022-06-18 12:42:31 +0200

    Updated Name Factory, Repository and Service

--- src/main/java/za/ac/cput/Repository/impl/NameRepository.java
+++ src/main/java/za/ac/cput/Repository/impl/NameRepository.java
@@ -6,16 +6,66 @@ import org.springframework.stereotype.Repository;
 import za.ac.cput.Domain.Name;
 import za.ac.cput.Repository.Interfaces.INameRepository;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-@Repository
-public abstract class NameRepository implements INameRepository {
+public class NameRepository implements INameRepository {
+
+    private Set<Name> names;
+    private static INameRepository nameRepository;
+
+    private NameRepository(){
+        this.names = new HashSet<>();
+    }
+
+    public static INameRepository getNameRepository(){
+        if (nameRepository == null){
+            nameRepository =  new NameRepository();
+        }
+        return nameRepository;
+    }
 
 
-    public Name retrieveByLastName (String lastname){
+    @Override
+    public Name create(Name name) {
 
-        List<Name> names = findAll();
+        this.names.add(name);
 
-        return names.stream().filter(name -> name.getLastName().equalsIgnoreCase(lastname)).findAny().orElse(null);
+        return name;
+
+    }
+
+    @Override
+    public Name read(String s) {
+
+        Name name = this.names.stream().filter( e -> e.getLastName().equalsIgnoreCase(s)).findAny().orElse(null);
+
+        return name;
+
+    }
+
+    @Override
+    public Name update(Name name) {
+
+        Name name1 = read(name.getMiddleName());
+
+        if (name1 != null) {
+            Name updated = new Name.Builder().copy(name1).setMiddleName(name.getMiddleName()).build();
+            name1 = updated;
+        }
+        return name1;
+
+    }
+
+    @Override
+    public void delete(String s) {
+
+        Name name = read(s);
+        this.names.remove(name);
+    }
+
+    public Set<Name> getAll() {
+
+        return names;
     }
 }
